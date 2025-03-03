18b6004b68e380b8d18f731d9141409a26c54698
TeddyDommie <mathemagician056@gmail.com>
2025-03-04 01:09:01 +0300

    Update channel.js

--- Marevick_cmds/channel.js
+++ Marevick_cmds/channel.js
@@ -34,31 +34,23 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
-𝚸𝚯𝐖𝚵𝚪𝚵𝐃 𝚩𝐘 𝚫𝚴𝐃𝚩𝚫𝐃-𝚻𝚵𝚫𝚳🇹🇿 
+POWER BY MZAZI-NET TECH KENYA 
 ╭─────────────────
 │❒⁠⁠⁠⁠╭─────────────
-│❒⁠⁠⁠⁠│▸ *𝐓𝐇𝐈𝐒 𝐀𝐑𝐄 𝐌𝐑 𝐀𝐍𝐃𝐁𝐀𝐃 𝐆𝐑𝐎𝐔𝐏 𝐋𝐈𝐍𝐊𝐒* 
-│❒⁠⁠⁠⁠│▸ *CHANNELS* 
-│❒⁠⁠⁠⁠│▸ *GROUPS*
+│❒⁠⁠⁠⁠│▸ *𝐓𝐇𝐈𝐒 IS MAVERICK-MD CHANNEL LINK AND GROUP LINK* 
+│❒⁠⁠⁠⁠│▸ *GROUP* 
+│❒⁠⁠⁠⁠│▸ *CHANNEL*
 │❒⁠⁠⁠⁠╰──────────────
-│❒⁠⁠⁠⁠│▸ *CHANNEL* :  https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m
-│❒⁠⁠⁠⁠│▸ *CHANNEL* :  https://whatsapp.com/channel/0029VaFytPbAojYm7RIs6l1x
-│❒⁠⁠⁠⁠│▸
-│❒⁠⁠⁠⁠│▸ 1. https://chat.whatsapp.com/K9VJDBIwGLc8sWszcEOhjo
-│❒⁠⁠⁠⁠│▸ 2. https://chat.whatsapp.com/LeXEFfLFyprAMI5rlBdfRj
-│❒⁠⁠⁠⁠│▸ 3. https://chat.whatsapp.com/KQUhHDtpBxUKfhl94OlxEf
-│❒⁠⁠⁠⁠│▸ 4. https://chat.whatsapp.com/Ilh736URrxrFUqp4qhRu7L
-│❒⁠⁠⁠⁠│▸ 5. https://chat.whatsapp.com/DU79JfdnOI83ZFVAyD74Mo
-│❒⁠⁠⁠⁠│▸ 6. https://chat.whatsapp.com/KX34mhuzDgYEU2vD2LAnty
-│❒⁠⁠⁠⁠│▸ 7. https://chat.whatsapp.com/BfxM1Xk8aMV8kkps5NshND
-│❒⁠⁠⁠⁠│▸ *MAKE SURE YOU HAVE JOINED THIS GROUPS*
+│❒⁠⁠⁠⁠│▸ *GROUP* :  https://chat.whatsapp.com/F0VwTbu4wDlD5weh68x3aC
+│❒⁠⁠⁠⁠│▸ *CHANNEL* :  https://whatsapp.com/channel/0029Vb7pEw24Crfinj6SfU2b
+│❒⁠⁠⁠⁠│▸ *MAKE SURE YOU HAVE JOINED THIS GROUP AND FOLLOWED THE CHANNEL*
 │❒⁠⁠⁠⁠│▸ 
 │❒⁠⁠⁠⁠╰──────────────
 ╰──────────────────\n
   `;
     
 let menuMsg = `
- ☠️𝐌𝐀𝐃𝐄 𝐄𝐀𝐒𝐘 𝐛𝐲 𝐌𝐑 𝐀𝐍𝐃𝐁𝐀𝐃☠️
+ ☠️MADE EASY BY MZAZI-NET TECH KENYA ☠️
 
 ❒────────────────────❒`;
 
