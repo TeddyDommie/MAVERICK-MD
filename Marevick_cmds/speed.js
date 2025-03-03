98134314567a8d5de4cb9ac147f91fd501bb4151
TeddyDommie <mathemagician056@gmail.com>
2025-03-04 01:29:01 +0300

    Update speed.js

--- Marevick_cmds/speed.js
+++ Marevick_cmds/speed.js
@@ -21,23 +21,23 @@ zokou({
   const _0x451c3b = _0x5a815c.map(_0x526b71 => "🟢 PONG: " + _0x526b71 + " 🟢").join("\n");
   const _0x41d2b1 = {
     'externalAdReply': {
-      'title': "ANDBAD-MD My Ultra Speed Response",
+      'title': "MAVERICK-MD My Ultra Speed Response",
       'body': "Ping Results : " + _0x451c3b,
-      'sourceUrl': "https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m",
-      'thumbnailUrl': "https://files.catbox.moe/sbkf12.jpg",
+      'sourceUrl': "https://whatsapp.com/channel/0029Vb7pEw24Crfinj6SfU2b",
+      'thumbnailUrl': "https://files.catbox.moe/wmbpkc.jpg",
       'mediaType': 0x1,
       'showAdAttribution': true
     }
   };
   const _0x54ea81 = {
-    'displayName': "ANDBAD-MD Support",
-    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: ANDBAD-MD\nORG: ANDBAD-MD Nexus;\nTEL;type=CELL;type=VOICE;waid=255764182801:+255 752 593 977\nEND:VCARD"
+    'displayName': "MAVERICK-MD Support",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: MAVERICK-MD\nORG: MAVERICK-MD Nexus;\nTEL;type=CELL;type=VOICE;waid=254100156216:+254 741 388986\nEND:VCARD"
   };
   await _0x41d5d3.sendMessage(_0x4cfdc6, {
-    'text': "⏰ *𝐂.𝚵.𝚯 𝚫𝚴𝐃𝚩𝚫𝐃* ✅",
+    'text': "⏰ *MZAZI-NET TECH KENYA* ✅",
     'contextInfo': _0x41d2b1,
     'contacts': {
-      'displayName': "NJABULO JB Verified Contact",
+      'displayName': "POLLIE Verified Contact",
       'contacts': [_0x54ea81]
     },
     'quoted': _0x2c968b
@@ -59,23 +59,23 @@ zokou({
   const _0x2f3abc = new Date(_0x766aaf * 1000).toISOString().substr(11, 8);
   const _0xae590 = {
     'externalAdReply': {
-      'title': "ANDBAD-MD- System Uptime",
+      'title': "MAVERICK-MD- System Uptime",
       'body': "Bot has been running for: " + _0x2f3abc,
-      'sourceUrl': "https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m",
-      'thumbnailUrl': "https://files.catbox.moe/sbkf12.jpg",
+      'sourceUrl': "https://whatsapp.com/channel/0029Vb7pEw24Crfinj6SfU2b",
+      'thumbnailUrl': "https://files.catbox.moe/wmbpkc.jpg",
       'mediaType': 0x1,
       'showAdAttribution': true
     }
   };
   const _0x297b69 = {
-    'displayName': "ANDBAD MD Support",
-    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: ANDBAD MD\nORG: ANDBAD-MD Nexus;\nTEL;type=CELL;type=VOICE;waid=255764182801:+244 752 593 977\nEND:VCARD"
+    'displayName': "MAVERICK-MD Support",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: MAVERICK-MD\nORG: MAVERICK-MD Nexus;\nTEL;type=CELL;type=VOICE;waid=254741388986:+254100156216\nEND:VCARD"
   };
   await _0x161605.sendMessage(_0x205206, {
-    'text': "*𝐂.𝚵.𝚯 𝚫𝚴𝐃𝚩𝚫𝐃 UPTIME* " + _0x2f3abc,
+    'text': "*MAVERICK-MD UPTIME* " + _0x2f3abc,
     'contextInfo': _0xae590,
     'contacts': {
-      'displayName': "ANDBAD Verified Contact",
+      'displayName': "POLLIE Verified Contact",
       'contacts': [_0x297b69]
     },
     'quoted': _0x4e9ae0
